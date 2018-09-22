03b5d55635f03ae6af6d93aed45d0199e146a52d
Peter Weyand <pweyand@gmail.com>
2018-09-21 22:33:40 -0500

    needle etc

--- frontend/src/components/subComponents/Home/BlogFile.vue
+++ frontend/src/components/subComponents/Home/BlogFile.vue
@@ -5,6 +5,15 @@
       Blog
     </h2>
     <h3>
+      September 21, 2018
+    </h3>
+    <p>
+      Morning - began work, nothing to report.
+    </p>
+    <p>
+      Evening - moved the needle.
+    </p>
+    <h3>
       September 20, 2018
     </h3>
     <p>
